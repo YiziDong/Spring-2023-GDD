18e1e9937b345cbacf8f0fd095f0fc2ecacae89d
XuanmingDong <yizi.666@berkeley.edu>
2023-05-24 22:15:41 -0700

    finished creating the script for camera switch to door

--- Assets/Scripts/UI/camera_switch_gate.cs
+++ Assets/Scripts/UI/camera_switch_gate.cs
@@ -7,15 +7,23 @@ public class camera_switch_gate : MonoBehaviour
     public Transform target;
     public Transform player;
     public bool SpawnersDead;
+    [SerializeField] private Animator openDoor;
+    [SerializeField] private string doorOpen = "openDoorAnimation";
 
-
-    private void OnEnable() {
-        float Tspeed = System.Math.Min(System.Math.Abs(target.position.x - player.position.x) + System.Math.Abs(target.position.y - player.position.y), 5f);
+    private void OnEnable()
+    {
+        float Tspeed = 5f;
         Vector3 targetPos = new Vector3(target.position.x, target.position.y, -10f);
         Vector3 originalPos = new Vector3(player.position.x, player.position.y, -10f);
-        transform.position = Vector3.Slerp(transform.position, targetPos, Tspeed*Time.deltaTime);
-        yield return new WaitForSeconds(3);
-        transform.position = Vector3.Slerp(transform.position, originalPos, Tspeed*Time.deltaTime);
+        //pause the game
+        Time.timeScale = 0;
+        transform.position = Vector3.Slerp(transform.position, targetPos, Tspeed*Time.unscaledDeltaTime);
+        //need to stay at the gate position for about 3s
+        openDoor.Play(doorOpen, 0, (System.Math.Abs(targetPos.x - originalPos.x) + System.Math.Abs(targetPos.y - originalPos.y))/5);
+        transform.position = Vector3.Slerp(transform.position, originalPos, Tspeed*Time.unscaledDeltaTime);
+        //resume the game again
+        Time.timeScale = 1;
     }
-    
+
+   
 }
